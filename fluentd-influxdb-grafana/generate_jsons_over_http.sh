ea5bc045733afe9caafe7d4d545e18fd281553f5
Joshua Tsang <joshua.tsang@soundmouse.com>
2022-10-30 17:53:25 +0000

    finally got grafana plotting a series of jsons auto generated from a bash script, but there's still a good 1 min delay between plotting and running the for loop

--- fluentd-influxdb-grafana/generate_jsons_over_http.sh
+++ fluentd-influxdb-grafana/generate_jsons_over_http.sh
@@ -1,3 +1,4 @@
-for i in {1..1000}; do echo \{\"time\":$(($(date +"%s")+$i)),\"foo\":\"foo\",\"bar\":$RANDOM\} >> logs/logs.json; sleep 1; done
+#curl -X POST -d 'json={"time":1667149355,"foo":"foo","bar":1270}' http://localhost:24224/default.logs -v
 
-curl -X POST -d 'json={"time":1667149355,"foo":"foo","bar":1270}' http://localhost:24224/default.logs -v
+
+for i in {1..2000}; do time_now=$(date +%s); curl -X POST -d 'json={"time":'${time_now}',"foo":"foo","bar":'${RANDOM}'}' http://localhost:24224/default.logs -v; sleep 1; done
