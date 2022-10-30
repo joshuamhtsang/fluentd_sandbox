9695c9b22a7fba2bf530bdb3672606f195d463af
Joshua Tsang <joshua.tsang@soundmouse.com>
2022-10-30 17:57:19 +0000

    first set of curves come out at 66 secs

--- fluentd-influxdb-grafana/generate_jsons_over_http.sh
+++ fluentd-influxdb-grafana/generate_jsons_over_http.sh
@@ -1,4 +1,4 @@
 #curl -X POST -d 'json={"time":1667149355,"foo":"foo","bar":1270}' http://localhost:24224/default.logs -v
 
 
-for i in {1..2000}; do time_now=$(date +%s); curl -X POST -d 'json={"time":'${time_now}',"foo":"foo","bar":'${RANDOM}'}' http://localhost:24224/default.logs -v; sleep 1; done
+for i in {1..2000}; do time_now=$(date +%s); curl -X POST -d 'json={"time":'${time_now}',"foo":"foo","bar":'${RANDOM}'}' http://localhost:24224/default.logs -v; sleep 1; echo $i; done
